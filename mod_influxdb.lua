2e6051bd6c26cd6ef4c00dc201793ed9d00e37fe
Marcus Stong <stongo@gmail.com>
2014-11-06 21:35:18 -0500

    refactor for multiple points and columns, add eventlog-stat event hook, remove anonymous stuff

--- mod_influxdb.lua
+++ mod_influxdb.lua
@@ -21,31 +21,23 @@ local prefix = (options.prefix or "prosody") .. "."
 
 local anonymous = options.anonymous or false
 
--- UUID generator
-local random = math.random
-local function uuid()
-    local template ='xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'
-    return string.gsub(template, '[xy]', function (c)
-        local v = (c == 'x') and random(0, 0xf) or random(8, 0xb)
-        return string.format('%x', v)
-    end)
-end
-
 -- Metrics are namespaced by ".", and seperated by newline
 function clean(s)
-    if anonymous == true then return uuid()
-    else return (s:gsub("[%.:\n]", "_")) end
+    return (s:gsub("[%.:\n]", "_"))
 end
 
 -- Standard point formatting
 function prepare_point(name, point, host)
   local hostname = host or module.host
+  table.insert(point.points, host)
+  if point.columns then
+    table.insert(point.columns, "host")
+  end
+
   local point_serialized = {
     name = prefix..name,
-    columns = { "value", "host" },
-    points = {
-      { point, hostname }
-    }
+    columns = point.columns or { "value", "host" },
+    points = { point.points }
   }
   return point_serialized
 end
@@ -64,71 +56,71 @@ end);
 -- count bare sessions every time, as we have no way to tell if it's a new bare session or not
 module:hook("resource-bind", function(event)
     local message = {}
-    table.insert(message, prepare_point("bare_sessions", iterators.count(pairs(bare_sessions))))
-    table.insert(message, prepare_point("full_sessions", 1))
+    table.insert(message, prepare_point("bare_sessions", { points = { iterators.count(pairs(bare_sessions)) }}))
+    table.insert(message, prepare_point("full_sessions", { points = { 1 }}))
     send(cjson.encode(message))
 end, 1)
 
 module:hook("resource-unbind", function(event)
     local message = {}
-    table.insert(message, prepare_point("bare_sessions", iterators.count(pairs(bare_sessions))))
-    table.insert(message, prepare_point("full_sessions", 1))
+    table.insert(message, prepare_point("bare_sessions", { points = {iterators.count(pairs(bare_sessions)) }}))
+    table.insert(message, prepare_point("full_sessions", { points = { 1 }}))
     send(cjson.encode(message))
 end, 1)
 
 -- Track MUC occupants as they join/leave
 module:hook("muc-occupant-joined", function(event)
     local message = {}
-    table.insert(message, prepare_point("n_occupants", 1))
+    table.insert(message, prepare_point("n_occupants", { points = { 1 }}))
     local room_node = jid.split(event.room.jid)
-    table.insert(message, prepare_point(clean(room_node)..".occupants", 1))
+    table.insert(message, prepare_point(clean(room_node)..".occupants", { points = { 1 }}))
     send(cjson.encode(message))
 end)
 
 module:hook("muc-occupant-left", function(event)
     local message = {}
-    table.insert(message, prepare_point("n_occupants", -1))
+    table.insert(message, prepare_point("n_occupants", { points = { -1 }}))
     local room_node = jid.split(event.room.jid)
     local occupants_name = clean(room_node)..".occupants"
-    table.insert(message, prepare_point(occupants_name, -1))
+    table.insert(message, prepare_point(occupants_name, { points = { -1 }}))
     send(cjson.encode(message))
 end)
 
 -- Misc other MUC
 module:hook("muc-broadcast-message", function(event)
     local message = {}
-    table.insert(message, prepare_point("broadcast-message", 1))
+    table.insert(message, prepare_point("broadcast-message", { points = { 1 }}))
     local room_node = jid.split(event.room.jid)
     local broadcast_message_name = clean(room_node)..".broadcast-message"
-    table.insert(message, prepare_point(broadcast_message_name, 1))
+    table.insert(message, prepare_point(broadcast_message_name, { points = { 1 }}))
     send(cjson.encode(message))
 end)
 
 module:hook("muc-invite", function(event)
     local message = {}
     -- Total count
-    table.insert(message, prepare_point("invite", 1))
+    table.insert(message, prepare_point("invite", { points = { 1 }}))
     local room_node = jid.split(event.room.jid)
     -- Counts per room
     local invite_name = clean(room_node)..".invite"
-    table.insert(message, prepare_point(invite_name, 1))
+    table.insert(message, prepare_point(invite_name, { points = { 1 }}))
     -- Counts per recipient
     local stanza_invite_name = clean(event.stanza.attr.to)..".invited"
-    table.insert(message, prepare_point(stanza_invite_name, 1))
+    table.insert(message, prepare_point(stanza_invite_name, { points = { 1 }}))
     send(cjson.encode(message))
 end)
 
 module:hook("muc-decline", function(event)
     local message = {}
     -- Total count
-    table.insert(message, prepare_point("decline", 1))
+    table.insert(message, prepare_point("decline", { points = { 1 }}))
     local room_node = jid.split(event.room.jid)
     -- Counts per room
     local room_decline_name = clean(room_node)..".decline"
-    table.insert(message, prepare_point(room_decline_name, 1))
+    table.insert(message, prepare_point(room_decline_name, { points = { 1 }}))
     -- Counts per sender
     local event_declined_name = clean(event.incoming.attr.from)..".declined"
-    table.insert(message, prepare_point(event_declined_name, 1))
+    table.insert(message, prepare_point(event_declined_name, { points = { 1 }}))
     send(cjson.encode(message))
 end)
 
@@ -140,9 +132,18 @@ module:hook("colibri-stats", function(event)
     for k,v in ipairs(event.stats) do
         local name = "jvb."..v.attr.name
         local value = tonumber(v.attr.value)
-        table.insert(message, prepare_point(name, value, host))
+        table.insert(message, prepare_point(name, { points = { value } }, host))
     end
-    --module:log("debug", "point %s: ", serialization.serialize(message))
+    send(cjson.encode(message))
+end)
+
+module:hook("eventlog-stat", function(event)
+    local message = {}
+    -- iterate over each stat
+    local name = event.metric
+    local room = event.room
+    local value = tonumber(event.value)
+    table.insert(message, prepare_point(name, { columns = { "room", "value" }, points = { room, value } }))
     send(cjson.encode(message))
 end)
 
